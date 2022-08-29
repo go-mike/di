2e5ee71f8cb6e64d2b269113f46a2548f1aba5d3
Iskander <iskander.sierra@gmail.com>
2022-08-29 23:09:07 +0200

    feat: Start working on ServiceCollection

--- pkg/di/ServiceCollection.go
+++ pkg/di/ServiceCollection.go
@@ -1,4 +1,49 @@
 package di
 
-type ServiceCollection interface {
+// ServiceCollection is a collection of services, describing a dependency graph of services.
+type ServiceCollection struct {
+	descriptors []ServiceDescriptor
+}
+
+// NewServiceCollection creates a new ServiceCollection.
+func NewServiceCollection() *ServiceCollection {
+	return &ServiceCollection{
+		descriptors: []ServiceDescriptor{},
+	}
+}
+
+// func (services *ServiceCollection) UpdateServices(
+// 	predicate func(ServiceDescriptor) bool,
+// 	update func([]ServiceDescriptor) []ServiceDescriptor) *ServiceCollection {
+// 	indices := []int{}
+// 	toReplace := []ServiceDescriptor{}
+// 	for i, descriptor := range services.descriptors {
+// 		if predicate(descriptor) {
+// 			indices = append(indices, i)
+// 			toReplace = append(toReplace, descriptor)
+// 		}
+// 	}
+
+// 	updated := update(toReplace)
+
+// 	if len(indices) == 0 {
+// 		services.descriptors = append(services.descriptors, updated...)
+// 	} else {
+// 		descriptors := make([]ServiceDescriptor, len(services.descriptors)-len(indices)+len(updated))
+// 		originalPos, indicesPos, updatedPos := 0, 0, 0
+
+// 		for i := 0; i < len(descriptors); i++ {
+// 			if 
+// 		}
+// 	}
+// 	return services
+// }
+
+func (services *ServiceCollection) Add(descriptor ServiceDescriptor) *ServiceCollection {
+	services.descriptors = append(services.descriptors, descriptor)
+	return services
+}
+
+func (services *ServiceCollection) Build() (ServiceProvider, error) {
+	panic("not implemented")
 }
